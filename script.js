dc871e5904f5d601e0be3d3c107681c40315af55
Anindita Priyadarshini <71321405+ANINDITAPRIYADARSHINI@users.noreply.github.com>
2025-04-05 05:58:16 +0000

    Till variable

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello!');
+//document.getElementById("count-el").innerText=5;//It says hey, html document, i want to get the element by its id
+
+// let count = 0 // It means "let count be zero"
+
+// console.log(count)
+
+let myAge = 27
+
+let humanDogRatio = 7
+
+let myDogAge = myAge * humanDogRatio
+
+console.log(myDogAge)
